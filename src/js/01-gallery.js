c3f2489f2d6af4767264c691d847ab08e75a2200
SedaNoty <sedanoty5@gmail.com>
2022-12-17 18:39:25 +0200

    create - hw-js-08_part-1
    create hw-js-08_part-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryPictures = document.querySelector('.gallery')
+const pictures = []
+
+galleryItems.forEach(element => {
+	const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = element.original
+	const galleryImage = document.createElement('img')
+	galleryImage.className = 'gallery__image'
+	galleryImage.src = element.preview
+	galleryImage.setAttribute('title', element.description)
+	galleryImage.alt = element.description
+
+	galleryLink.append(galleryImage)
+	pictures.push(galleryLink)
+})
+galleryPictures.append(...pictures)
+
+new SimpleLightbox('.gallery a', {
+    captionsData: `alt`,
+	captionDelay: 250,
+});
